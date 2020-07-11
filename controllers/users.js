2d7a1605475cbcb8c51328c9421da5f792d0496d
aayanksinghai <aayanksinghai02@gmail.com>
2020-07-12 03:21:31 +0530

    MongoDB Setup

---
+++ controllers/users.js
@@ -0,0 +1,6 @@
+//@desc Save User Details
+//@route POST /api/v1/users
+//@access Public
+exports.saveUserDetails = (req, res, next) => {
+    res.status(200).json({ success: true })
+}
--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const connectDB = require('./config/db')
 
 // Load env vars
 dotenv.config({ path: './config/config.env' })
 
+//Connect to database
+connectDB()
+
 const app = express()
+
+//Dev logging middleware
+if(process.env.NODE_ENV === 'development')
+{
+    app.use(morgan('dev'))
+}
+// Mount Routers
+app.use('/api/v1/users', require('./routes/users'))
+
 
 const PORT = process.env.PORT || 8080
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`)
+    //Close server & exit process
+    server.close(() => process.exit(1))
+})
