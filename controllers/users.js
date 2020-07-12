7925fa1d310f88c91ebf509e3eb9e5e6ae25a3ca
aayanksinghai <aayanksinghai02@gmail.com>
2020-07-13 01:23:38 +0530

    Image File Upload

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const User = require('../models/Users')
+const path = require('path')
 
 
 //@desc Save User Details
@@ -6,15 +7,42 @@ const User = require('../models/Users')
 //@access Public
 exports.saveUserDetails = async (req, res, next) => {
     try {
-        console.log(req.body)
-        //res.status(200).json({ success: true, data: user })
-        
-        const user = await User.create(req.body)
+        //console.log(req.body)
+        if(!req.files)
+        {
+            return res.status(400).json({ success: false, data: 'Please upload a file'})
+        }
 
-        res.status(201).json({ success: true, data: user })
+        const file = req.files.file
+
+        // Make sure the image is a photo
+        if(!file.mimetype.startsWith('image'))
+        {
+            return res.status(400).json({ success: false, data: 'Please upload an image file'})
+        }
+
+        //Check filesize
+        if(file.size > process.env.MAX_FILE_UPLOAD){
+            return res.status(400).json({ success: false, data: `Please upload an image less than ${process.env.MAX_FILE_UPLOAD}`})
+        }
+
+        // Create custom filename
+        file.name = `photo_${req.body.Name}${path.parse(file.name).ext}`
+        file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async err =>{
+            if(err){
+                console.error(err)
+                return res.status(500).json({ success: false, data: 'Problem with file upload'})
+            }
+            req.body.Photo = file.name
+            
+            const user = await User.create(req.body)
+            res.status(201).json({ success: true, data: user })
         
-        } 
+        })
+     
+    } 
     catch (err) {
+        console.error(err)
         res.status(400).json({ success: false, data: err.message})
     }
 }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const fileupload = require('express-fileupload')
 const connectDB = require('./config/db')
 
 // Load env vars
@@ -19,6 +20,10 @@ if(process.env.NODE_ENV === 'development')
 {
     app.use(morgan('dev'))
 }
+
+//File Uploading
+app.use(fileupload())
+
 // Mount Routers
 app.use('/api/v1/users', require('./routes/users'))
 
